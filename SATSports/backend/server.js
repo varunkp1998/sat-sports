684a5182e4e8f8eab8dac41de2d2982d1a61b452
varunkp1998 <54853805+varunkp1998@users.noreply.github.com>
2026-04-01 16:16:52 +0530

    new push

--- SATSports/backend/server.js
+++ SATSports/backend/server.js
@@ -88,7 +88,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-
+const getDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a = 
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
+};
 
 
 let isAuthenticated = false;
